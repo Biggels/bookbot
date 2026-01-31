3d10f53f9e8f4b8ce4a045c6c47fe3365b5d9ad9
rhaack-subseven <rhaack@subseven.net>
2026-01-30 21:14:19 -0500

    Refactor main to print book contents from file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+
+if __name__ == "__main__":
+    main()
