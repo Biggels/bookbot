8f81530fddf0a1d8617f90debe79db946a0afac0
rhaack-subseven <rhaack@subseven.net>
2026-01-31 17:12:26 -0500

    Update hardcoded path in book report to match path from argument instead

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
     )
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
